611bf542ac4a1e554012372464198af40c2e4967
aouchu <katherinerivera.balut@gmail.com>
2024-10-30 01:57:21 +0800

    SQL

--- librarysystem (2).sql
+++ librarysystem (2).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Oct 26, 2024 at 02:22 AM
+-- Generation Time: Oct 29, 2024 at 05:56 PM
 -- Server version: 8.0.30
 -- PHP Version: 8.1.10
 
@@ -44,7 +44,7 @@ CREATE TABLE `book` (
 --
 
 INSERT INTO `book` (`BookID`, `Title`, `Author`, `ISBN`, `Publisher`, `YearPublished`, `Quantity`, `Pages`, `Status`) VALUES
-(14, 'Lester', 'Caibal', 4, 'c', 5, 6, 7, 'Reserved'),
+(14, 'Lester', 'Caibal', 4, 'c', 5, 6, 7, 'Checked Out'),
 (15, 'Mi', 'b', 4, 'c', 5, 6, 7, 'Checked Out'),
 (16, 'Shiro', 'Caibal', 4, 'c', 5, 6, 7, 'Reserved'),
 (17, 'Myx', 'Les', 1, 'Lala', 2, 3, 4, 'Available'),
@@ -60,15 +60,53 @@ INSERT INTO `book` (`BookID`, `Title`, `Author`, `ISBN`, `Publisher`, `YearPubli
 
 CREATE TABLE `borrow` (
   `id` int NOT NULL,
-  `reservation_id` int NOT NULL,
-  `borrow_date` date NOT NULL,
-  `return_date` date NOT NULL,
-  `due_date` date NOT NULL,
-  `status` varchar(20) NOT NULL,
-  `fines` int DEFAULT NULL,
-  `payment_date` date DEFAULT NULL,
-  `notes` text NOT NULL
+  `BorrowDate` date NOT NULL,
+  `DueDate` date NOT NULL,
+  `FineAmount` double DEFAULT NULL,
+  `DateFineIssued` date DEFAULT NULL,
+  `PaymentDate` date DEFAULT NULL,
+  `FineNotes` varchar(100) DEFAULT NULL,
+  `ReservationDate` date NOT NULL,
+  `ReservationExpiryDate` date NOT NULL,
+  `PickupDate` date NOT NULL,
+  `ReservationNotes` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `BookID` int DEFAULT NULL,
+  `StudentID` int DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `borrow`
+--
+
+INSERT INTO `borrow` (`id`, `BorrowDate`, `DueDate`, `FineAmount`, `DateFineIssued`, `PaymentDate`, `FineNotes`, `ReservationDate`, `ReservationExpiryDate`, `PickupDate`, `ReservationNotes`, `BookID`, `StudentID`) VALUES
+(1, '2024-10-31', '2024-11-04', 0, NULL, NULL, 'Test', '2024-10-29', '2024-10-31', '2024-10-31', 'For REFERENCES', 14, 2),
+(2, '2024-10-31', '2024-11-03', 0, NULL, NULL, 'None', '2024-10-29', '2024-10-31', '2024-10-31', 'For SCHOOL PROJECTS', 14, 2),
+(5, '2024-10-31', '2024-11-02', 0, NULL, NULL, '', '2024-10-29', '2024-10-31', '2024-10-31', 'For SCHOOL ASSIGNMENT', 18, 2),
+(6, '2024-11-02', '2024-11-05', NULL, NULL, NULL, NULL, '2024-10-30', '2024-11-02', '2024-11-03', 'For PROJECT', 17, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `librarian`
+--
+
+CREATE TABLE `librarian` (
+  `LibrarianID` int NOT NULL,
+  `Fullname` varchar(100) NOT NULL,
+  `Gender` varchar(10) NOT NULL,
+  `Email` varchar(100) NOT NULL,
+  `ContactNumber` varchar(20) NOT NULL,
+  `EmergencyNumber` varchar(20) NOT NULL,
+  `LibrarianAddress` varchar(100) NOT NULL,
+  `UserID` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `librarian`
+--
+
+INSERT INTO `librarian` (`LibrarianID`, `Fullname`, `Gender`, `Email`, `ContactNumber`, `EmergencyNumber`, `LibrarianAddress`, `UserID`) VALUES
+(1, 'Katherine Rivera', 'Female', 'katherine@gmail.com', '09999999999', '09999999998', 'TEST', 2);
 
 -- --------------------------------------------------------
 
@@ -99,30 +137,6 @@ INSERT INTO `register` (`user_id`, `username`, `password`, `email`, `fullname`, 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `reservation`
---
-
-CREATE TABLE `reservation` (
-  `id` int NOT NULL,
-  `student_id` int NOT NULL,
-  `book_id` int NOT NULL,
-  `reservation_date` date NOT NULL,
-  `expiry_date` date NOT NULL,
-  `pickup_date` date NOT NULL,
-  `return_date` date NOT NULL,
-  `notes` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Dumping data for table `reservation`
---
-
-INSERT INTO `reservation` (`id`, `student_id`, `book_id`, `reservation_date`, `expiry_date`, `pickup_date`, `return_date`, `notes`) VALUES
-(1, 2, 14, '2024-10-27', '2024-10-28', '2024-10-28', '2024-10-31', 'PROCESSING');
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `students`
 --
 
@@ -144,7 +158,8 @@ CREATE TABLE `students` (
 --
 
 INSERT INTO `students` (`StudentID`, `FullName`, `Gender`, `Email`, `ContactNumber`, `EmergencyNumber`, `Program`, `YearLevel`, `StudentAddress`, `user_id`) VALUES
-(2, 'Lester Caibal', 'Male', 'lestercaibal@gmail.com', '09912736453', '09987654323', 'BSIT', 1, 'Pambisan', 1);
+(2, 'Lester Caibal', 'Male', 'lestercaibal@gmail.com', '09912736453', '09987654323', 'BSIT', 1, 'Pambisan', 1),
+(3, 'Joyce ', 'Female', 'Joyce@gmail.com', '09123456789', '09987654321', 'STEM', 3, 'Calapan', 4);
 
 --
 -- Indexes for dumped tables
@@ -160,7 +175,16 @@ ALTER TABLE `book`
 -- Indexes for table `borrow`
 --
 ALTER TABLE `borrow`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `BookID` (`BookID`),
+  ADD KEY `StudentID` (`StudentID`);
+
+--
+-- Indexes for table `librarian`
+--
+ALTER TABLE `librarian`
+  ADD PRIMARY KEY (`LibrarianID`),
+  ADD KEY `UserID` (`UserID`);
 
 --
 -- Indexes for table `register`
@@ -169,19 +193,12 @@ ALTER TABLE `register`
   ADD PRIMARY KEY (`user_id`);
 
 --
--- Indexes for table `reservation`
---
-ALTER TABLE `reservation`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `book_id` (`book_id`),
-  ADD KEY `student_id` (`student_id`);
-
---
 -- Indexes for table `students`
 --
 ALTER TABLE `students`
   ADD PRIMARY KEY (`StudentID`),
   ADD UNIQUE KEY `Email` (`Email`),
+  ADD UNIQUE KEY `unique_userid` (`user_id`),
   ADD KEY `user_id` (`user_id`);
 
 --
@@ -198,7 +215,13 @@ ALTER TABLE `book`
 -- AUTO_INCREMENT for table `borrow`
 --
 ALTER TABLE `borrow`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `librarian`
+--
+ALTER TABLE `librarian`
+  MODIFY `LibrarianID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `register`
@@ -207,27 +230,27 @@ ALTER TABLE `register`
   MODIFY `user_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
--- AUTO_INCREMENT for table `reservation`
---
-ALTER TABLE `reservation`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
 -- AUTO_INCREMENT for table `students`
 --
 ALTER TABLE `students`
-  MODIFY `StudentID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `StudentID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `reservation`
+-- Constraints for table `borrow`
 --
-ALTER TABLE `reservation`
-  ADD CONSTRAINT `reservation_ibfk_1` FOREIGN KEY (`book_id`) REFERENCES `book` (`BookID`),
-  ADD CONSTRAINT `reservation_ibfk_2` FOREIGN KEY (`student_id`) REFERENCES `students` (`StudentID`);
+ALTER TABLE `borrow`
+  ADD CONSTRAINT `borrow_ibfk_1` FOREIGN KEY (`BookID`) REFERENCES `book` (`BookID`),
+  ADD CONSTRAINT `borrow_ibfk_2` FOREIGN KEY (`StudentID`) REFERENCES `students` (`StudentID`);
+
+--
+-- Constraints for table `librarian`
+--
+ALTER TABLE `librarian`
+  ADD CONSTRAINT `librarian_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `register` (`user_id`);
 
 --
 -- Constraints for table `students`
